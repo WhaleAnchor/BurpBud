05239bb3d6fe544273212e7f401a7be542ade09f
Whale Anchor <andrewsungwoochoi@gmail.com>
2023-03-27 23:04:46 -0700

    cleaned up unused code

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,12 @@
-import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { auth, logInWithEmailAndPassword, signInWithGoogle } from "../firebase/firebase";
-import { useAuthState } from "react-firebase-hooks/auth";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { logInWithEmailAndPassword, signInWithGoogle } from "../firebase/firebase";
 import "./Login.css";
 
 function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [user, loading, error] = useAuthState(auth);
-    const navigate = useNavigate();
-    useEffect(() => {
-        if (loading) {
-        // maybe trigger a loading screen
-        return;
-        }
-        if (user) navigate("/logs");
-    }, [user, loading]);
+    
     return (
         <div className="login">
         <div className="login__container">
