fc046152bf09215ed97e7216eccc889450c6246b
Whale Anchor <andrewsungwoochoi@gmail.com>
2023-03-27 18:58:54 -0700

    fixed values and added additional functionalities

--- src/components/BurpLogs.js
+++ src/components/BurpLogs.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { collection, addDoc, doc, setDoc } from "firebase/firestore";
-import {db, auth, addBurpLog} from "../firebase/firebase";
+import {db, auth, addBurpLog, getLastBurpLog} from "../firebase/firebase";
 import './BurpLogs.css'
 
 // material ui imports
@@ -25,34 +25,61 @@ const getCurrentDateInMMDDYYYYFormat = () => {
     return `${month}/${day}/${year}`;
 };
 
-const BurpLogs = ({ uid, lastBurpTime }) => {
+
+const BurpLogs = ({ uid }) => {
     const [newBurpTime, setNewBurpTime] = useState(getCurrentTimeInMilitaryFormat());
-    const [newBurpCount, setNewBurpCount] = useState('');
-    const [newBurpDuration, setNewBurpDuration] = useState(lastBurpTime ? (Date.now() - lastBurpTime) / 60000 : ''); // Calculate delta in minutes
+    const [newBurpCount, setNewBurpCount] = useState(1);
     const [newBurpDate, setNewBurpDate] = useState(getCurrentDateInMMDDYYYYFormat());
     const [newBurpComment, setNewBurpComment] = useState('');
-  
+    const [lastBurpTime, setLastBurpTime] = useState(null);
+    const [newBurpDuration, setNewBurpDuration] = useState(''); 
+    // find the most recent log 
+    useEffect(() => {
+        if (uid) {
+            (async () => {
+                const lastLogTime = await getLastBurpLog(uid);
+                setLastBurpTime(lastLogTime);
+            })();
+        }
+    }, [uid]);
+
+    useEffect(() => {
+        if (lastBurpTime) {
+          const now = new Date();
+          const currentTimeInMinutes = now.getHours() * 60 + now.getMinutes();
+          const [lastHours, lastMinutes] = lastBurpTime.split(':').map(Number);
+          const lastTimeInMinutes = lastHours * 60 + lastMinutes;
+          const duration = currentTimeInMinutes - lastTimeInMinutes;
+          setNewBurpDuration(duration);
+        }
+    }, [lastBurpTime]);
+
+    const timeSinceLastLog = () => {
+        if (!lastBurpTime) {
+            return 'N/A';
+        }
+    
+        const now = new Date();
+        const lastLogTime = new Date(lastBurpTime);
+        const diffInMinutes = Math.floor((now - lastLogTime) / 60000);
+        const hours = Math.floor(diffInMinutes / 60);
+        const minutes = diffInMinutes % 60;
+    
+        return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+    };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-
-        console.log("uid:", uid);
-        console.log("newBurpTime:", newBurpTime);
-        console.log("newBurpCount:", newBurpCount);
-        console.log("newBurpDuration:", newBurpDuration);
-        console.log("newBurpDate:", newBurpDate);
-        console.log("newBurpComment:", newBurpComment);
         // Call the addBurpLog function to add the data to Firestore
         await addBurpLog(uid, newBurpTime, newBurpCount, newBurpDuration, newBurpDate, newBurpComment);
     
         // Clear the input fields after submitting
         setNewBurpTime(getCurrentTimeInMilitaryFormat());
         setNewBurpCount('');
-        setNewBurpDuration('');
+        setNewBurpDuration(timeSinceLastLog());
         setNewBurpDate(getCurrentDateInMMDDYYYYFormat());
         setNewBurpComment('');
     };
-
-    
 
     // Styling
     const boxStyles = { '& > :not(style)': { m: 1 } };
@@ -64,13 +91,25 @@ const BurpLogs = ({ uid, lastBurpTime }) => {
                 <div className="inputFields">
                     Current Time | {getCurrentDateInMMDDYYYYFormat()} | {getCurrentTimeInMilitaryFormat()}
                     <Box component="Count" sx={boxStyles} noValidate autoComplete="off">
-                        <TextField id="outlined-basic" label="Count" variant="outlined" onChange={(e) => setNewBurpCount(e.target.value)} />
+                        <TextField
+                            id="outlined-basic"
+                            label="Count"
+                            variant="outlined"
+                            value={newBurpCount}
+                            onChange={(e) => setNewBurpCount(e.target.value)}
+                        />
                     </Box>
                     <Box component="Comment" sx={boxStyles} noValidate autoComplete="off">
-                        <TextField id="outlined-basic" label="Comment" variant="outlined" onChange={(e) => setNewBurpComment(e.target.value)} />
+                        <TextField 
+                            id="outlined-basic" 
+                            label="Comment" 
+                            variant="outlined" 
+                            onChange={(e) => setNewBurpComment(e.target.value)} />
                     </Box>
                     <Stack spacing={2} direction="row" className="logButton">
-                        <Button variant="contained" onClick={handleSubmit}>Log Burp</Button>
+                        <Button variant="contained" onClick={handleSubmit}>
+                            Log Burp
+                        </Button>
                     </Stack>
                 </div>
             </Grid>
