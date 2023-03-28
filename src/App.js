bd26ce832ec19229636b8d95e30871d540bfdfee
Whale Anchor <andrewsungwoochoi@gmail.com>
2023-03-27 23:23:55 -0700

    found the bug- wrong path way

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { auth } from "../firebase/firebase";
+import { auth } from "./firebase/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 
