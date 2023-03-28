052e4336fb4a4407f90b4d7f7b930a79100a64ef
Whale Anchor <andrewsungwoochoi@gmail.com>
2023-03-27 18:16:21 -0700

    added a few routes

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from './components/Login'
 import Register from './components/Register';
 import Reset from './components/Reset';
-import InventoryPage from './pages/InventoryPage';
+import BurpLogs from './components/BurpLogs';
+import NavBar from './components/NavBar';
 
 
 function App() {
   return (
     <Router>
+      <NavBar/>
       <Routes>
         <>
           <Route exact path="/" element={<Login />} />
           <Route exact path="/register" element={<Register />} />
           <Route exact path="/reset" element={<Reset />} />
-          <Route exact path="/inventory" element={<InventoryPage />} />
+          <Route exact path="/logs" element={<BurpLogs />} />
         </>
       </Routes>
     </Router>
