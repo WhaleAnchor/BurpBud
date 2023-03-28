9179a8dea535ca9ee4cd2d7ae68657fb67add394
Whale Anchor <andrewsungwoochoi@gmail.com>
2023-03-27 20:18:00 -0700

    added state for changing components on loggingpage.js

--- src/pages/LoggingPage.js
+++ src/pages/LoggingPage.js
@@ -2,9 +2,15 @@ import React, { useState, useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase/firebase";
 import BurpLogs from "../components/BurpLogs";
+import BurpButton from "../components/BurpButton";
+import './LoggingPage.css'
+import Button from '@mui/material/Button';
+import Timeline from '../components/Timeline';
 
-const LoggingPage = () => {
+const LoggingPage = ({ activeComponent }) => {
   const [user, setUser] = useState(null);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 600);
+  const [showBurpLogs, setShowBurpLogs] = useState(!isMobile);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -14,16 +20,50 @@ const LoggingPage = () => {
     return () => unsubscribe();
   }, []);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 600);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    setShowBurpLogs(!isMobile);
+  }, [isMobile]);
+
+  const toggleBurpLogs = () => {
+    setShowBurpLogs(!showBurpLogs);
+  };
+
   return (
-    <div>
+    <div className="LoggingPageWrapper">
       {user ? (
         <>
           <h1>Welcome, {user.displayName}</h1>
           {/* Pass the user's UID as a prop to the BurpLogs component */}
-          <BurpLogs uid={user.uid} />
+          <div className="LogContainer">
+            {activeComponent === 'logs' && (
+              <>
+                {showBurpLogs || !isMobile ? <BurpLogs uid={user.uid} /> : null}
+                <BurpButton uid={user.uid}/>
+              {isMobile && (
+                <Button variant="outlined" onClick={toggleBurpLogs}>
+                  {showBurpLogs ? "Hide BurpLogs" : "or Manually Log"}
+                </Button>
+              )}
+            </>
+            )}
+            {activeComponent === 'timeline' && <Timeline />}
+          </div>
         </>
       ) : (
         <h1>Please sign in</h1>
+        
       )}
     </div>
   );
