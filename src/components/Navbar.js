3a6b44e7b947e3c7834ba5044faaaf03dddf595c
Whale Anchor <andrewsungwoochoi@gmail.com>
2023-03-27 22:44:37 -0700

    debug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ import { useNavigate } from "react-router-dom";
 import { auth, logout } from "../firebase/firebase";
 
 
-function NavBar({ setActiveComponent }) {
+export function NavBar({ setActiveComponent }) {
   const [user, loading, error] = useAuthState(auth);
   const navigate = useNavigate();
   
