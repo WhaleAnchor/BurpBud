49f84f6f308c4bf4ad2965569200a23dffc8993c
Whale Anchor <andrewsungwoochoi@gmail.com>
2023-03-27 20:16:08 -0700

    added state to change components for logging / timeline

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Login from './components/Login'
@@ -6,18 +7,20 @@ import Register from './components/Register';
 import Reset from './components/Reset';
 import LoggingPage from './pages/LoggingPage';
 import NavBar from './components/NavBar';
-
+import Timeline from './components/Timeline';
 
 function App() {
+  const [activeComponent, setActiveComponent] = useState('logs');
+
   return (
     <Router>
-      <NavBar/>
+      <NavBar setActiveComponent={setActiveComponent} />
       <Routes>
         <>
           <Route exact path="/" element={<Login />} />
           <Route exact path="/register" element={<Register />} />
           <Route exact path="/reset" element={<Reset />} />
-          <Route exact path="/logs" element={<LoggingPage />} />
+          <Route exact path="/logs" element={<LoggingPage activeComponent={activeComponent} />} />
         </>
       </Routes>
     </Router>
