17d8452821099ee994333c2a481d822404e09b70
Whale Anchor <andrewsungwoochoi@gmail.com>
2023-03-29 15:53:20 -0700

    added text on login and sizing

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,45 +9,43 @@ function Login() {
     
     return (
         <div className="login">
-        <div className="login__container">
-
-            <input
-                type="text"
-                className="login__textBox"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="E-mail Address"
-            />
-
-            <input
-                type="password"
-                className="login__textBox"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Password"
-            />
-            
-            <button
-                className="login__btn"
-                onClick={() => logInWithEmailAndPassword(email, password)}
-                >
-                Login
-            </button>
-
-            <button 
-                className="login__btn login__google" 
-                onClick={signInWithGoogle}>
-                Login with Google
-            </button>
-
-            <div>
-                <Link to="/reset">Forgot Password</Link>
+            <h1>Welcome to BurpBud!</h1>
+            <div className="login__container">
+                <input
+                    type="text"
+                    className="login__textBox"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="E-mail Address"
+                />
+                <input
+                    type="password"
+                    className="login__textBox"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Password"
+                />
+                <button
+                    className="login__btn"
+                    onClick={() => logInWithEmailAndPassword(email, password)}
+                    >
+                    Login
+                </button>
+                <button 
+                    className="login__btn login__google" 
+                    onClick={signInWithGoogle}>
+                    Login with Google
+                </button>
+                <div>
+                    <Link to="/reset">Forgot Password</Link>
+                </div>
+                <div>
+                    Don't have an account? <Link to="/register">Register</Link> now.
+                </div>
             </div>
-            <div>
-                Don't have an account? <Link to="/register">Register</Link> now.
-            </div>
-            
-        </div>
+            <span className="spanTip">
+                Register and Login through Google for better experience
+            </span>
         </div>
     );
 }
